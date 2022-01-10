9c7ba8b3d38e56903e1f669e37a330d0755a1831
Juan Arguelles <juannarguelles@gmail.com>
2022-01-10 17:58:35 -0300

    agregado el nabar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home';
+import Navbar from './components/navbar/index'
 
 function App() {
   return (
-    <div className="App">
-    Hola
-    </div>
+      <Router>
+        <Navbar/>
+          <Switch>
+            <Route path='/' exact component={Home} />      
+          </Switch>
+      </Router>
   );
 }
 
-export default App;
+export default App; 
---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const home = () => {
+    return (
+        <div>
+            hola
+        </div>
+    )
+}
+
+export default home
